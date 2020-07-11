a1310de5526bc8558e234929f6a5c33361e0bdd7
Romain Fouilland <romain.fd@gmail.com>
2020-07-12 00:21:50 +0100

    Indented operator string

--- playground.py
+++ playground.py
@@ -1,7 +1,41 @@
-from qiskit.aqua.operators import MatrixOp
-from qiskit.aqua.operators import X, Z
+from qiskit.circuit.library import HGate
+
+from qiskit.aqua.operators import MatrixOp, ComposedOp, SummedOp, X, Z, I, OperatorStateFn, CircuitStateFn, PrimitiveOp
 
 if __name__ == '__main__':
+    print(
+        ComposedOp([
+            SummedOp([
+                ComposedOp([
+                    OperatorStateFn(
+                        SummedOp(
+                            [0.18093119978423136 * Z,
+                             -1.052373245772859 * I
+                             ],
+                            abelian=True
+                        ),
+                        is_measurement=True
+                    ),
+                    PrimitiveOp(HGate())
+                ]),
+                ComposedOp([
+                    OperatorStateFn(SummedOp(
+                            [0.18093119978423136 * Z,
+                             -1.052373245772859 * I
+                             - 1.052373245772859 * I
+                             ],
+                            abelian=True
+                        ),
+                        is_measurement=True
+                    ),
+                    I
+                ])
+            ]),
+            PrimitiveOp(HGate())
+        ])
+    )
+
+    # https://github.com/Qiskit/qiskit-aqua/pull/1111 -> Matrix multiplicative factor (z + z == 2 * z)
     z = MatrixOp([[1, 0], [0, -1]])
     a = Z + z
     b = z + Z

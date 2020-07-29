75305dde9eb0e1c8d1345e6c7b2883b6d9d30c2a
Romain Fouilland <romain.fd@gmail.com>
2020-07-29 23:08:47 +0100

    Updating test and moving INDENTATION in class to avoid import cycle

--- playground.py
+++ playground.py
@@ -3,6 +3,18 @@ from qiskit.circuit.library import HGate
 from qiskit.aqua.operators import MatrixOp, ComposedOp, SummedOp, X, Z, I, OperatorStateFn, PrimitiveOp, ListOp
 
 if __name__ == '__main__':
+    op = MatrixOp([[1, 0], [0, -1]])
+    initial_str = str(op)
+    indented_str = op._indent(initial_str)
+    starts_with_indent = indented_str.startswith(op.INDENTATION)
+    assert starts_with_indent
+    indented_str_content = (indented_str[len(op.INDENTATION):] + op.INDENTATION).split("\n{}".format(op.INDENTATION))
+    print(indented_str)
+    print(initial_str)
+    print(indented_str_content)
+    print(initial_str.split("\n"))
+    assert indented_str_content == initial_str.split("\n")
+
     assert "\t\tString\n\t\tto indent\n" == ListOp._indent("String\nto indent\n", indentation="\t\t")
 
     print(
